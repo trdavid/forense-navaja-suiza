a3d512d6125993cc9650891d3b244c14259f0825
David Triana <david.triana@scj.gov.co>
2020-11-21 18:09:20 -0500

    [BAT] Se agrega submenu del item 1 generar fecha

--- toolkit.bat
+++ toolkit.bat
@@ -30,9 +30,39 @@ echo.
 goto:inicio
 :op1
 echo.
-echo. Has elegido la opcion No. 1
+echo. Has elegido la opcion No. 1 Información Volátil
+echo ------------------------------------------------------------------------------
+echo 1 Generar la Fecha
+echo 2 Generar la Hora
+echo 3 Lista de Procesos
+echo 4 Lista de Librerias
+echo 5 Todas las anteriores
+echo 6 Salir
+echo ------------------------------------------------------------------------------
 echo.
 ::Aquí van las líneas de comando de tu opción
+	SET /p var1= ^> Seleccione una opcion [1-6]:
+
+	if "%var1%"=="0" goto inicio1
+	if "%var1%"=="1" goto op1_1
+	if "%var1%"=="2" goto op1_2
+	if "%var1%"=="3" goto op1_3
+	if "%var1%"=="4" goto op1_4
+	if "%var1%"=="5" goto op1_5
+	if "%var1%"=="6" goto salir
+::Mensaje de error, validación cuando se selecciona una opción fuera de rango
+echo. El numero "%var1%" no es una opcion invalida, por favor intente de nuevo.
+echo.
+pause
+echo.
+goto:inicio1
+:op1_1
+echo.
+echo. Has elegido la opcion No. 1.1 Generar Fecha
+cd heramientas_sys_internals
+date /t > ..\resultados_volatiles\fecha.txt
+echo. ya se creo el archivo en la carpeta resultados_volatiles
+::cierre de la opcion 1
 color 08
 echo.
 pause
